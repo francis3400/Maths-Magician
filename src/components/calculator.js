ab2ad6a46344da624a42e4559d49a36a377c8a29
francis3400 <francisuche712@gmail.com>
2023-02-17 14:00:00 -0800

    added events

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,68 +1,199 @@
+import { useState } from 'react';
+import calculate from './logic/calculate';
+
 function Calculator() {
+  const [result, setResult] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (buttonName) => {
+    setResult(calculate(result, buttonName));
+  };
+
   return (
     <div className="App">
       <div className="calculator">
-        <div className="display">0</div>
+        <div className="display">
+          <div className="display-screen" type="text">
+            {result.next || result.total || '0'}
+          </div>
+        </div>
 
         <div className="operators">
-          <button type="button" className="ope Divide">
+          <button
+            type="button"
+            onClick={() => {
+              handleClick('÷');
+            }}
+            className="ope Divide"
+          >
             ÷
           </button>
-          <button type="button" className="ope Multiply">
+          <button
+            type="button"
+            onClick={() => {
+              handleClick('x');
+            }}
+            className="ope Multiply"
+          >
             x
           </button>
-          <button type="button" className="ope Minus">
+          <button
+            type="button"
+            onClick={() => {
+              handleClick('-');
+            }}
+            className="ope Minus"
+          >
             -
           </button>
-          <button type="button" className="ope Plus">
+          <button
+            type="button"
+            onClick={() => {
+              handleClick('+');
+            }}
+            className="ope Plus"
+          >
             +
           </button>
-          <button type="button" className="ope Equal">
+          <button
+            type="button"
+            onClick={() => {
+              handleClick('=');
+            }}
+            className="ope Equal"
+          >
             =
           </button>
         </div>
 
         <div className="digits">
-          <button type="button" className="delete delete">
+          <button
+            type="button"
+            onClick={() => {
+              handleClick('AC');
+            }}
+            className="delete delete"
+          >
             AC
           </button>
-          <button type="button" className="delete clear">
+          <button
+            type="button"
+            onClick={() => {
+              handleClick('+/-');
+            }}
+            className="delete clear"
+          >
             +/-
           </button>
-          <button type="button" className="percentage">
+          <button
+            type="button"
+            onClick={() => {
+              handleClick('%');
+            }}
+            className="percentage"
+          >
             %
           </button>
-          <button type="button" className="seven">
+          <button
+            type="button"
+            onClick={() => {
+              handleClick('7');
+            }}
+            className="seven"
+          >
             7
           </button>
-          <button type="button" className="eight">
+          <button
+            type="button"
+            onClick={() => {
+              handleClick('8');
+            }}
+            className="eight"
+          >
             8
           </button>
-          <button type="button" className="nine">
+          <button
+            type="button"
+            onClick={() => {
+              handleClick('9');
+            }}
+            className="nine"
+          >
             9
           </button>
-          <button type="button" className="four">
+          <button
+            type="button"
+            onClick={() => {
+              handleClick('4');
+            }}
+            className="four"
+          >
             4
           </button>
-          <button type="button" className="five">
+          <button
+            type="button"
+            onClick={() => {
+              handleClick('5');
+            }}
+            className="five"
+          >
             5
           </button>
-          <button type="button" className="six">
+          <button
+            type="button"
+            onClick={() => {
+              handleClick('6');
+            }}
+            className="six"
+          >
             6
           </button>
-          <button type="button" className="one">
+          <button
+            type="button"
+            onClick={() => {
+              handleClick('1');
+            }}
+            className="one"
+          >
             1
           </button>
-          <button type="button" className="two">
+          <button
+            type="button"
+            onClick={() => {
+              handleClick('2');
+            }}
+            className="two"
+          >
             2
           </button>
-          <button type="button" className="three">
+          <button
+            type="button"
+            onClick={() => {
+              handleClick('3');
+            }}
+            className="three"
+          >
             3
           </button>
-          <button type="button" className="zero">
+          <button
+            type="button"
+            onClick={() => {
+              handleClick('0');
+            }}
+            className="zero"
+          >
             0
           </button>
-          <button type="button" className="decimal">
+          <button
+            type="button"
+            onClick={() => {
+              handleClick('.');
+            }}
+            className="decimal"
+          >
             .
           </button>
         </div>
